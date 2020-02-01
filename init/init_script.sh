eeda621a570c2d28f69ea5a81cf2ece6281e9cff
ZyCromerZ <neetroid97@gmail.com>
2020-02-01 21:39:39 +0700

    init_script.sh add .git

--- init/init_script.sh
+++ init/init_script.sh
@@ -48,7 +48,7 @@ botclone() {
     cd ~
     echo "Cloning bot sources..."
     if [ -z "$bot_clone" ]; then
-        git clone https://github.com/ZyCromerZ/OpenUserBot-Telegram -b master
+        git clone https://github.com/ZyCromerZ/OpenUserBot-Telegram.git -b master
         save_prog "bot_clone"
     fi
     echo "DONE!!"
