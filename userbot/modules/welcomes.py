a28dc195100c710d7a86b85d52fd79ef9c9d4aac
ZyCromerZ <neetroid97@gmail.com>
2020-02-02 23:26:49 +0700

    wellcome.py fix BOT_NAME salah posisi

--- userbot/modules/welcomes.py
+++ userbot/modules/welcomes.py
@@ -58,8 +58,8 @@ async def welcome_to_chat(event):
                 current_saved_welcome_message = msg_o.message
             elif cws and cws.reply:
                 current_saved_welcome_message = cws.reply
+            global BOT_NAME
             current_message = await event.reply(
-                global BOT_NAME
                 current_saved_welcome_message.format(mention=mention,
                                                      title=title,
                                                      count=count,
