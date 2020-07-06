89a74fc0c5c2b1d0984b0e7abf3b967aca9f18d6
ZyCromerZ <neetroid97@gmail.com>
2020-07-06 20:58:20 +0700

    userbot: modules: qrcode: remove barcode commands

--- userbot/modules/qrcode.py
+++ userbot/modules/qrcode.py
@@ -11,8 +11,6 @@ import os
 import asyncio
 
 import qrcode
-import barcode
-from barcode.writer import ImageWriter
 
 from bs4 import BeautifulSoup
 
@@ -49,51 +47,6 @@ async def parseqr(qr_e):
     soup = BeautifulSoup(t_response, "html.parser")
     qr_contents = soup.find_all("pre")[0].text
     await qr_e.edit(qr_contents)
-
-
-@register(pattern=r"^.barcode(?: |$)([\s\S]*)", outgoing=True)
-async def barcode(event):
-    """ For .barcode command, genrate a barcode containing the given content. """
-    await event.edit("`Processing..`")
-    input_str = event.pattern_match.group(1)
-    message = "SYNTAX: `.barcode <long text to include>`"
-    reply_msg_id = event.message.id
-    if input_str:
-        message = input_str
-    elif event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        reply_msg_id = previous_message.id
-        if previous_message.media:
-            downloaded_file_name = await event.client.download_media(
-                previous_message)
-            m_list = None
-            with open(downloaded_file_name, "rb") as fd:
-                m_list = fd.readlines()
-            message = ""
-            for m in m_list:
-                message += m.decode("UTF-8") + "\r\n"
-            os.remove(downloaded_file_name)
-        else:
-            message = previous_message.message
-    else:
-        event.edit("SYNTAX: `.barcode <long text to include>`")
-        return
-
-    bar_code_type = "code128"
-    try:
-        bar_code_mode_f = barcode.get(bar_code_type,
-                                      message,
-                                      writer=ImageWriter())
-        filename = bar_code_mode_f.save(bar_code_type)
-        await event.client.send_file(event.chat_id,
-                                     filename,
-                                     reply_to=reply_msg_id)
-        os.remove(filename)
-    except Exception as e:
-        await event.edit(str(e))
-        return
-    await event.delete()
-
 
 @register(pattern=r"^.makeqr(?: |$)([\s\S]*)", outgoing=True)
 async def make_qr(makeqr):
@@ -141,13 +94,5 @@ CMD_HELP.update({
     ".makeqr <content>\
 \nUsage: Make a QR Code from the given content.\
 \nExample: .makeqr www.google.com\
-\nNote: use .decode <reply to barcode/qrcode> to get decoded content."
-})
-
-CMD_HELP.update({
-    'barcode':
-    ".barcode <content>\
-\nUsage: Make a BarCode from the given content.\
-\nExample: .barcode www.google.com\
 \nNote: use .decode <reply to barcode/qrcode> to get decoded content."
 })
