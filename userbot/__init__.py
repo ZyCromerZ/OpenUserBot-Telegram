3a9db04190a5fa0ce04845ab5b39f359fc012295
MoveAngel <moveangel@minaproject.tech>
2020-07-13 20:13:43 +0700

    init: alive_logo: set default logo/image alive
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -133,7 +133,7 @@ YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
 # Default .alive logo
-ALIVE_LOGO = os.environ.get("ALIVE_LOGO") or None
+ALIVE_LOGO = os.environ.get("ALIVE_LOGO") or "https://telegra.ph/file/bc4b184058f13932957d0.jpg"
 
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY", ""))
